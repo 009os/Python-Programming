f27da4904e8ba0d307c3725fd9e8da99968d27c9
OMJI SHUKLA <91083571+009os@users.noreply.github.com>
2023-05-28 21:56:19 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,5 @@
+#WRITE A PROGRAM TO TAKE YOUR NAME AND SURNAME AS INPUT AND PRINT YOUR NAME AS OUTPUT
+print("Welcome to PYTHON programming.")
+x=input("ENTER YOUR NAME : ")
+y=input("ENTER YOUR SURNAME : ")
+print("Details :",x,y)
