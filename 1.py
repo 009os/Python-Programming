b6aa1f66bf28563900168ed9294e8af24e7d1e3b
OMJI SHUKLA <91083571+009os@users.noreply.github.com>
2023-05-29 00:10:38 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,14 @@
+#1. Create an numpy array of 10 zeros.
+#2. Create an numpy array of 10 ones.
+#3. Create an numpy array of 10 fives.
+
+import numpy as np
+array=np.zeros(10)
+print(" 10 zeros:")
+print(array)
+array=np.ones(10)
+print(" 10 ones:")
+print(array)
+array=np.ones(10)*5
+print(" 10 fives:")
+print(array)
